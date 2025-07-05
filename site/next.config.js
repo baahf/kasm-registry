fa9754d14f4f0b40d3e337a797027e20a848a711
baahf <github@deepstar.net>
2025-07-05 19:57:52 +0200

    Update next.config.js
    Start site

--- site/next.config.js
+++ site/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     description: 'My Own version of some customized workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://baahf.github.io/kasm-registry/',
-    contactUrl: 'https://deepstar.net/support',
+    contactUrl: 'https://github.com/baahf/kasm-registry/issues',
   },
   reactStrictMode: true,
   basePath: '/kasm-registry/1.0',
